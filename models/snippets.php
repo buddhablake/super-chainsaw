3062d45de77e3426f951fbb71be0a0dc7ee7b4db
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-07-08 11:20:41 -0700

    POST, GET, AND UPDATE routes created and tested with postman.

--- models/snippets.php
+++ models/snippets.php
@@ -1,7 +1,9 @@
 <?php
 $dbconn = pg_connect("host=localhost dbname=snipsnap");
 
-
+//==============
+//SNIPPET CLASS
+//==============
 
 class Snippet {
   public $id;
@@ -20,9 +22,17 @@ class Snippet {
   }
 }
 
+//=================
+//SNIPPET FACTORY
+//=================
+
 
 class Snippets {
-  static functions all(){
+
+
+  //RETURNS ALL SNIPPETS
+
+  static function all(){
     $snippets = [];
 
     $results = pg_query("SELECT * FROM snippets");
@@ -40,6 +50,28 @@ class Snippets {
       $row_object = pg_fetch_object($results);
     }
     return $snippets;
+  }
+
+  //CREATES NEW SNIPPET
+  static function create($snippet){
+    $query = "INSERT INTO snippets (title, author, description, snippet) VALUES ($1, $2, $3, $4)";
+
+    $query_params = [$snippet->title, $snippet->author, $snippet->description, $snippet->snippet];
+
+    pg_query_params($query, $query_params);
+
+    return self::all();
+  }
+
+  //UPDATES A SNIPPET
+  static function update($updated_snippet){
+    $query = "UPDATE snippets SET title = $1, author = $2, description = $3, snippet = $4 WHERE id = $5";
+
+    $query_params = [$updated_snippet->title, $updated_snippet->author, $updated_snippet->description, $updated_snippet->snippet, $updated_snippet->id];
+
+    pg_query_params($query, $query_params);
+
+    return self::all();
   }
 }
  ?>
