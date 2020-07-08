2be61e782451463009e971b736d1b6df7cf33980
Tim <tsclay9@gmail.com>
2020-07-08 18:12:13 -0400

    progress on adding ; need to debug

--- components/NewSnippet.jsx
+++ components/NewSnippet.jsx
@@ -1,27 +1,64 @@
-class NewSnippet extends Component {
+class NewSnippet extends React.Component {
   constructor(props) {
     super(props);
-    this.changeTitle = this.props.changeTitle;
-    this.changeAuthor = this.props.changeAuthor;
-    this.changeDescription = this.props.changeDescription;
-    this.changeSnippet = this.props.changeSnippet;
+    const {
+      changeTitle,
+      changeAuthor,
+      changeDescription,
+      changeSnippet,
+      createSnippet
+    } = this.props;
+    this.changeTitle = changeTitle;
+    this.changeAuthor = changeAuthor;
+    this.changeDescription = changeDescription;
+    this.changeSnippet = changeSnippet;
+    this.createSnippet = createSnippet;
   }
+
+  handleTitle = (e) => {
+    this.changeTitle(e);
+  };
+
+  handleAuthor = (e) => {
+    this.changeAuthor(e);
+  };
+
+  handleDescription = (e) => {
+    this.changeDescription(e);
+  };
+
+  handleSnippet = (e) => {
+    this.changeSnippet(e);
+  };
+
+  handleCreate = (e) => {
+    e.preventDefault();
+    this.createSnippet();
+  };
 
   render() {
     return (
-      <div>
-        <form>
+      <div className="add-form">
+        <form onSubmit={this.handleCreate}>
           <input onKeyUp={this.handleTitle} type="text" placeholder="Title" />
           <input onKeyUp={this.handleAuthor} type="text" placeholder="Author" />
           <textarea
             onChange={this.handleDescription}
             name="new-description"
             id="new-description"
-            cols="30"
+            cols="50"
             rows="10"
             placeholder="What does the snippet do?"
           />
-          <textarea name="" id="" cols="30" rows="10" />
+          <textarea
+            onChange={this.handleSnippet}
+            name="new-snippet"
+            id="new-snippet"
+            cols="50"
+            rows="10"
+            placeholder="Insert your snippet here!"
+          />
+          <button type="submit">Add</button>
         </form>
       </div>
     );
